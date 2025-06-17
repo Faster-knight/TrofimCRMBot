a159971969aee4ed05dacebfbcdc4c98a9e817fd
Faster-knight <postnikovtrofim1@gmail.com>
2025-06-17 14:13:24 +0300

    Temp working commit

--- src/data.py
+++ src/data.py
@@ -1,16 +1,44 @@
-from json import dump, load
+from json import load, dumps
 
-def writeJsonObject(data: dict, pathFile: str) -> None:
-    pass
-
-
-def readJsonObject(pathFile: str) -> dict | list:
-    pass
+def writeJsonObject(data: dict, pathFile: str, encoding_name: str = "utf-8") -> None:
+    with open(pathFile, "w", encoding=encoding_name) as f:
+        dumps(fp=f, obj=data)
 
 
-def writeEnvData(data: dict, pathFile: str) -> None:
-    pass
+def readJsonObject(pathFile: str, encoding_name: str = "utf-8") -> dict | list:
+    with open(pathFile, "r", encoding=encoding_name) as f:
+        data = load(f)
+    return data
 
 
-def readEnvFile(pathFile: str) -> dict:
-    pass
+def writeEnvData(data: dict, pathFile: str, encoding_name: str = "utf-8") -> None:
+    __temp = str("")
+    for k, v in data:
+        __temp += f"{k}={v}\n"
+    with open(pathFile, "w", encoding=encoding_name) as f:
+        f.write(__temp)
+        del __temp
+        del k
+        del v
+
+
+def readEnvFile(pathFile: str, encoding_name: str = "utf-8") -> dict:
+    """
+    [RUSSIAN]
+    Этот метод парсит и получает данные из .env файла
+    возвращая объект словаря.
+    :param pathFile: str - путь к целевому файлу в файловой системе.
+    :param encoding_name: str - название кодировки в которой должен находится целевой файл.
+    :return: dict - данные полученные из файла.
+    """
+    with open(pathFile, "r", encoding=encoding_name) as f:
+        __temp = f.read()
+    __sp = __temp.split("\n")
+    del __temp
+    t = dict()
+    for line in __sp:
+        __temp = line.split("=")
+        t[__temp[0]] = __temp[1]
+    del __temp
+    del __sp
+    return t
